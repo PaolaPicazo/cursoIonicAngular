b34eefa5f1dd1421860193634dc88ff698349b25
PaolaPicazo <p_picazo@hotmail.es>
2022-08-04 15:09:32 -0500

    modulo

---
+++ src/app/paola/paola.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { PaolaPageRoutingModule } from './paola-routing.module';
+import { PaolaPage } from './paola.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PaolaPageRoutingModule
+  ],
+  declarations: [PaolaPage]
+})
+export class PaolaPageModule {
+
+}
---
+++ src/app/paola/paola.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+//  import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//  import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-paola',
+  templateUrl: './paola.page.html',
+  styleUrls: ['./paola.page.scss'],
+})
+
+
+
+export class PaolaPage implements OnInit {
+
+
+
+
+ ngOnInit() {
+   }
+
+
+
+}
